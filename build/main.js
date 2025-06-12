558eee328cc34b54256837903ec469b9ee7f6afc
Dinesh Budhathoki <physmarika@gmail.com>
2025-06-12 11:41:28 +0800

    Learning functions of the typescript

--- build/main.js
+++ build/main.js
@@ -1,3 +1,7 @@
 "use strict";
-let firstName = "Dinesh";
-console.log(firstName);
+const calculateTotalPrice = (quantity, price, tax) => {
+    const basePrice = quantity * price;
+    const taxAmount = basePrice * tax;
+    return basePrice + taxAmount;
+};
+console.log(calculateTotalPrice(10, 100, 0.1));
--- main.ts
+++ main.ts
@@ -1,2 +1,10 @@
-let firstName: string = "Dinesh";
-console.log(firstName);
+const calculateTotalPrice = (
+  quantity: number,
+  price: number,
+  tax: number
+): number => {
+  const basePrice = quantity * price;
+  const taxAmount = basePrice * tax;
+  return basePrice + taxAmount;
+};
+console.log(calculateTotalPrice(10, 100, 0.1));
