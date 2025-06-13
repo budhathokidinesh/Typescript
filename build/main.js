6b106205e9f22c3b99b2060c067ddb1592b88bf9
Dinesh Budhathoki <physmarika@gmail.com>
2025-06-13 22:44:36 +0800

    Live of typescript

--- build/main.js
+++ build/main.js
@@ -1,7 +1,2 @@
 "use strict";
-const calculateTotalPrice = (quantity, price, tax) => {
-    const basePrice = quantity * price;
-    const taxAmount = basePrice * tax;
-    return basePrice + taxAmount;
-};
-console.log(calculateTotalPrice(10, 100, 0.1));
+console.log(1);
