34bad26f5d6f09f8f723cce4adf722bdcac5a700
Dinesh Budhathoki <physmarika@gmail.com>
2025-06-18 15:19:57 +0800

    Phone diectory is done using typescript

--- build/main.js
+++ build/main.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log(1);
+class Contact {
+    constructor(id, name, phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+}
+let contacts = [];
+let contactId = 0;
+function addConantact() {
+    const nameInput = document.getElementById("name-input");
+    const phoneInput = document.getElementById("phone-input");
+    const name = nameInput.value;
+    const phone = parseInt(phoneInput.value);
+    if (name && phone) {
+        contacts.push(new Contact(contactId++, name, phone));
+        renderContact();
+        nameInput.value = "";
+        phoneInput.value = "";
+    }
+}
+function renderContact() {
+    const contactList = document.getElementById("contact-list");
+    contactList.innerHTML = "";
+    contacts.forEach((contact) => {
+        const listItem = document.createElement("li");
+        const divItem = document.createElement("div");
+        divItem.className = "item";
+        divItem.textContent = `${contact.name}: ${contact.phone}`;
+        listItem.appendChild(divItem);
+        contactList.appendChild(listItem);
+    });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,49 @@
-console.log(1);
+class Contact {
+  public id: number;
+  public name: string;
+  public phone: number;
+
+  constructor(id: number, name: string, phone: number) {
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+  }
+}
+let contacts: Contact[] = [];
+let contactId: number = 0;
+
+function addConantact(): void {
+  const nameInput: HTMLInputElement = document.getElementById(
+    "name-input"
+  ) as HTMLInputElement;
+  const phoneInput: HTMLInputElement = document.getElementById(
+    "phone-input"
+  ) as HTMLInputElement;
+  const name: string = nameInput.value;
+  const phone: number = parseInt(phoneInput.value);
+  if (name && phone) {
+    contacts.push(new Contact(contactId++, name, phone));
+    renderContact();
+    nameInput.value = "";
+    phoneInput.value = "";
+  }
+}
+
+function renderContact(): void {
+  const contactList: HTMLUListElement = document.getElementById(
+    "contact-list"
+  ) as HTMLUListElement;
+  contactList.innerHTML = "";
+  contacts.forEach((contact) => {
+    const listItem: HTMLLIElement = document.createElement(
+      "li"
+    ) as HTMLLIElement;
+    const divItem: HTMLDivElement = document.createElement(
+      "div"
+    ) as HTMLDivElement;
+    divItem.className = "item";
+    divItem.textContent = `${contact.name}: ${contact.phone}`;
+    listItem.appendChild(divItem);
+    contactList.appendChild(listItem);
+  });
+}
